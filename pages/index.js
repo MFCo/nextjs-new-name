eec8585f9bcc9bb81e12b19d2e7240718201caa8
Mariano Fernández Cocirio <marianococirio@gmail.com>
2024-01-22 11:24:06 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,7 @@ export default function Home() {
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
+            {console.log(process.env.TESTING_BRANCH)}
           </p>
           <div>
             <a
